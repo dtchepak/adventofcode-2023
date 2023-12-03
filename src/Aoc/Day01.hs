9e6569f1c5be6931cba83d77f180c9175c9ee8a4
David Tchepak <dave@davesquared.net>
2023-12-03 14:00:48 +1100

    Day 1 part 2

--- src/Aoc/Day01.hs
+++ src/Aoc/Day01.hs
@@ -1,14 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Aoc.Day01 (
   calibrations,
-  answers
+  calibrations',
+  firstDigit,
+  lastDigit,
+  day01
 ) where
 
+import Data.Bifunctor (first)
 import Data.Char (isDigit)
+import Data.List (minimumBy)
 import Data.Maybe (fromMaybe)
 import Text.Read (readMaybe)
+import qualified Data.Text as T
 
-readInput :: IO Int
-readInput = calibrations <$> readFile "data/day01.txt"
+readInput :: IO String
+readInput = readFile "data/day01.txt"
 
 digits :: [Char] -> [Char]
 digits = filter isDigit
@@ -18,15 +25,60 @@ calibrationLine s =
   let d = digits s
   in fromMaybe 0 (readMaybe [headOr '0' d, headOr '0' (reverse d)])
 
+headOr :: Foldable t => a -> t a -> a
+headOr = foldr const
+
+digitTokens :: [(T.Text, Int)]
+digitTokens =
+  [ ("0"    , 0)
+  , ("1"    , 1)
+  , ("2"    , 2)
+  , ("3"    , 3)
+  , ("4"    , 4)
+  , ("5"    , 5)
+  , ("6"    , 6)
+  , ("7"    , 7)
+  , ("8"    , 8)
+  , ("9"    , 9)
+  -- "zero" not defined in spec
+  , ("one"  , 1)
+  , ("two"  , 2)
+  , ("three", 3)
+  , ("four" , 4)
+  , ("five" , 5)
+  , ("six"  , 6)
+  , ("seven", 7)
+  , ("eight", 8)
+  , ("nine" , 9)
+  ]
+
+findTokenIndex :: T.Text -> T.Text -> Int
+findTokenIndex needle = T.length . fst . T.breakOn needle
+
+compareTokens :: T.Text -> (T.Text, a) -> (T.Text, b) -> Ordering
+compareTokens t a b = findTokenIndex (fst a) t `compare` findTokenIndex (fst b) t
+
+firstMatch :: T.Text -> [(T.Text, a)] -> a
+firstMatch t = snd . minimumBy (compareTokens t)
+
+firstDigit :: T.Text -> Int
+firstDigit t = firstMatch t digitTokens
+
+lastDigit :: T.Text -> Int
+lastDigit t = firstMatch (T.reverse t) (first T.reverse <$> digitTokens)
+
 calibrations :: String -> Int
 calibrations =
   sum . fmap calibrationLine . lines
 
-headOr :: Foldable t => a -> t a -> a
-headOr = foldr const
+calibrations' :: String -> Int
+calibrations' =
+  sum . fmap (\l -> firstDigit l * 10 + lastDigit l) . T.lines . T.pack
 
-answers :: IO ()
-answers = do
+day01 :: IO ()
+day01 = do
   input <- readInput
   putStrLn "Part 1:"
-  print input
+  print (calibrations input)
+  putStrLn "Part 2:"
+  print (calibrations' input)
--- test/Aoc/Day01Spec.hs
+++ test/Aoc/Day01Spec.hs
@@ -1,13 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Aoc.Day01Spec (spec) where
 
+import qualified Data.Text as T
 import Test.Hspec
 import Aoc.Day01
 
 part1Example :: String
 part1Example = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 
+part2Example :: String
+part2Example = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+
+part2Subset :: String
+part2Subset = "2eight8fmktlf\nvjchzt7btthreesix1tcngpbtzsfmvsx\nsjv8\nncqpkzh5twooneoneqfxlqbjjhqsrlkhvdnvtbzpcbj\n449three45three"
+
+getDigits :: String -> [Int]
+getDigits s =
+  let input = T.lines (T.pack s)
+  in (\a -> firstDigit a * 10 + lastDigit a) <$> input
+
 spec :: Spec
 spec = do
   describe "example data" $ do
     it "part 1: example" $
       calibrations part1Example `shouldBe` 142
+    it "part 2: firstDigits" $
+        firstDigit <$> T.lines (T.pack part2Example) `shouldBe` [2, 8, 1, 2, 4, 1, 7]
+    it "part 2: lastDigits" $
+        lastDigit  <$> T.lines (T.pack part2Example) `shouldBe` [9, 3, 3, 4, 2, 4, 6]
+    it "part 2: digits" $
+        getDigits part2Example `shouldBe` [29, 83, 13, 24, 42, 14, 76]
+    it "part 2: subset" $
+        getDigits part2Subset `shouldBe` [28, 71, 88, 51, 43]
+    it "part 2: one digit" $
+        getDigits "sjv8" `shouldBe` [88]
+    it "part 2: example" $
+      calibrations' part2Example `shouldBe` 281
